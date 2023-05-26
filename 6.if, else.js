572269f10570490fa8d63be33f31780d16e889bc
shinseogyong <108261108+shinseogyong@users.noreply.github.com>
2023-05-26 21:32:41 +0900

    if, else 조건문
    == 동등연산자는 문자형을 숫자형으로
    자동형변환.

--- 6.if, else.js
+++ 6.if, else.js
@@ -6,6 +6,14 @@ const b = '1';
 console.log(a==b); //동등비교 연산자는 자동형변환 시, 문자 -> 숫자로 변환한다.
 console.log(a===b);
 
-//조건문
+//잘 쓰이는 if조건문
 
+let myCharmingPoint = 20;
+if(myCharmingPoint >= 80) {
+  console.log(`당신은 윤윤씨의 사랑을 듬뿍 받게 될 것입니다`);
+} else if (myCharmingPoint >= 50){
+  console.log(`당신은 윤윤씨의 사랑을 받기에 조금 어리숙합니다`);
+} else {
+  console.log(`당신은 전혀 매력이 없습니다. 노력하세요`);
+}
 
