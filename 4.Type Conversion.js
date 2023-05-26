8da23a1d9d5b0e505f7802496337fd89ff9f6fc5
shinseogyong <108261108+shinseogyong@users.noreply.github.com>
2023-05-26 20:31:13 +0900

    자동형변환
    Number()
    String()
    Boolean()
    그리고
    Number(null) // 0
    Number(undefined) // NaN

---
+++ 4.Type Conversion.js
@@ -0,0 +1,17 @@
+const mathScore = prompt("수학 몇점?");
+const engScore = prompt("영어 몇점?");
+const result = (mathScore + engScore) / 2; 
+
+console.log(result); //== 9080 / 2 = 4540 -> 자동형변환
+
+//prompt로 입력받은 값은 무조건 '문자형' 입니다.
+
+//그래서 명시적 형변환 필요
+String()
+Number()
+Boolean() //값이 false가 되는 경우? [0, 빈문자열'', null, undefined, NaN]
+
+
+//타입변환 규칙에 따른 특이사항, 암기 필요
+Number(null); //0
+Number(undefined); //NaN, 숫자로 변환될 수 없는 값이기에 NaN
